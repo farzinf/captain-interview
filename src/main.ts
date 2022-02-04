2f3afb55ed4f87b51e1ace6afe577571ec857a78
farzin <farzin.falahati@gmail.com>
2022-02-04 16:56:59 +0330

    use validation pipe

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle('Captain API')
     .setDescription('The captain interview test API documentation')
