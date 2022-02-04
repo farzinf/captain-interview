a6fe01892fe08871db2552571a9301038aa9d8b0
farzin <farzin.falahati@gmail.com>
2022-02-04 14:18:24 +0330

    import mongoose module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
-
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`,
+    ),
+  ],
   controllers: [],
   providers: [],
 })
