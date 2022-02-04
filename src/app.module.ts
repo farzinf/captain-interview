714a57bdae51d57953ea0183d16d66cb95e31eea
farzin <farzin.falahati@gmail.com>
2022-02-04 21:10:41 +0330

    import modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
 import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './users/user.module';
+import { AuthModule } from './auth/auth.module';
+import { PaymentModule } from './payments/payment.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(
       `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`,
     ),
+    UserModule,
+    AuthModule,
+    PaymentModule,
   ],
   controllers: [],
   providers: [],
