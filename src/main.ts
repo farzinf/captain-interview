9494ec5af68c8f07b03f76b179454837bac77af5
farzin <farzin.falahati@gmail.com>
2022-02-04 14:45:23 +0330

    setup swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Captain API')
+    .setDescription('The captain interview test API documentation')
+    .setVersion('1.0')
+    .addTag('Users')
+    .addTag('Payments')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  const swaggerOptions = {
+    swaggerOptions: { docExpansion: 'none' },
+  };
+  SwaggerModule.setup('api', app, document, swaggerOptions);
   await app.listen(+process.env.PORT);
 }
 bootstrap();
